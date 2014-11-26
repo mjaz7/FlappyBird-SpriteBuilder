ac6f46616b500c407dc14627bb2b507bf6a739df
mjaz7 <manaljaz@gmail.com>
2014-11-26 16:28:38 +0000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -16,11 +16,13 @@
     // put update code here
 }
 
+
 // put new methods here
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
+    [self addObstacle];
 }
 
 @end
